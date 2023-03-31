3636375698bc27c3aca2dbdba7f76446ba0b14fc
subhajit100 <asubhajit35@gmail.com>
2023-03-31 20:37:06 +0530

    remove react routing for github deployment

--- src/App.js
+++ src/App.js
@@ -1,15 +1,15 @@
 import React, { useState } from "react";
 import "./App.css";
 import Alert from "./components/Alert";
-import About from "./components/About";
+// import About from "./components/About";
 import Navbar from "./components/Navbar";
 import TextForm from "./components/TextForm";
 
-import {
-  BrowserRouter as Router,
-  Routes,
-  Route
-} from "react-router-dom";
+// import {
+//   BrowserRouter as Router,
+//   Routes,
+//   Route
+// } from "react-router-dom";
 function App() {
   const [mode, setMode] = useState("light"); // whether dark mode enabled or not
   const [alert, setAlert] = useState(null); // setting the alert object msg and type based on dark or light mode
@@ -42,7 +42,7 @@ function App() {
 
   return (
     <>
-      <Router>
+      {/* <Router> */}
       <Navbar  title="MyTextUtils"
           aboutText="MyAbout"
           homeText="MyHome"
@@ -50,14 +50,16 @@ function App() {
           toggleMode={toggleVisibilityMode}/>
       <Alert alert={alert} />
       <div className="container my-3">
-        <Routes>   
-          <Route exact path="/about" element={<About />} />
-          <Route exact path="/" element={<TextForm heading="Enter text for analysis here below"
+        {/* <Routes>    */}
+          {/* <Route exact path="/about" element={<About />} /> */}
+          {/* <Route exact path="/" element={ */}
+          <TextForm heading="Enter text for analysis here below"
                 mode={mode}
-                showAlert={showAlert}/>} />   
-        </Routes>
+                showAlert={showAlert}/>
+                {/* } />    */}
+        {/* </Routes> */}
       </div>
-   </Router>
+   {/* </Router> */}
     </>
   );
 }
